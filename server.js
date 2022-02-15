06480886bf42d458f2f35d3e7f408dceb40249c2
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2022-02-15 11:17:01 -0800

    created server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'); // import express
+
+// initialize app variable with express
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('API running')
+})
+
+// look for an environment variable named PORT so when depoyed to heroku will look for it.
+// if there is no environment varibale set it will run on port 5000
+const PORT = process.env.PORT || 5000;
+
+// make app listen on a port
+app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
